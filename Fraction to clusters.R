c1a958f0dd1af4b7eb5fa62d085655f931e1f40c
wp1g19 <63693169+wp1g19@users.noreply.github.com>
2024-02-05 09:22:30 +0000

    Delete Fraction to clusters.R

--- Fraction to clusters.R
+++
@@ -1,7 +0,0 @@
-clusterfractional1 <- read.csv(file = "C:/Users/wp1g19/Documents/Revisit bioinformatics/Digital cytometry/TPM rerun/CIBERSORTx_Fractional.csv", header = TRUE, row.names = 1)
-rownames(clusterfractional1) <- gsub("/",".", rownames(clusterfractional1))
-rownames(clusterfractional1) <- gsub("-",".", rownames(clusterfractional1))
-
-wr <- merge(clustersFract, clusterfractional1, by = 0)
- write.csv(x = wr, file = "C:/Users/wp1g19/Documents/Revisit bioinformatics/Digital cytometry/TPM rerun/CIBERSORTx_Fractional_withclusters.csv")
- 
